23c4e31ed381e662965296cddd005703c6388567
João <Joao.victornn@Yahoo.com.br>
2026-09-08 17:02:29 -0300

    Esqueleto do front: Index e estilo

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const db= require('./db');
 const app = express();
 app.use(express.json());
+app.use(express.static('public'));
 const PORT = 3000;
 
 app.get('/', (req, res) => {
